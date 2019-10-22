8eb114ff9a6ee698571a37824f4bedf801d799e5
N15H4Nt <56861578+N15H4Nt@users.noreply.github.com>
2019-10-22 12:52:22 +0530

    Update guessanumber.py

--- guessanumber.py
+++ guessanumber.py
@@ -13,9 +13,6 @@ while(noOFguesses<=10):
     elif(guessNum >randomVal):
         print("You enter a greater no.,input a smaller no. ")
     
-    elif(guessNum >=101):
-        print("Enter a value from 0 to 100")
-    
     else:
         print("Congratulations, you won!!!")
         break
