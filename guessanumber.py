8b3aa72e451c8be5928018add0e76078f04b39e1
N15H4Nt <56861578+N15H4Nt@users.noreply.github.com>
2019-10-22 12:07:42 +0530

    Add files via upload

---
+++ guessanumber.py
@@ -0,0 +1,23 @@
+import random
+randomVal = random.randint(0, 100)
+noOFguesses = 1
+print("((GUESS A NUMBER)) "
+      "Created By - Nishant Tiwari")
+print("Try to guess a number from (0 to 100)")
+while(noOFguesses<=10):
+    guessNum = int(input("Enter a number"))
+
+    if(guessNum <randomVal):
+        print("You enter a smaller no.,input a greater no.")
+
+    elif(guessNum >randomVal):
+        print("You enter a greater no.,input a smaller no. ")
+
+    else:
+        print("Congratulations, you won!!!")
+        break
+
+    print(10 - noOFguesses, "No. of guess left")
+    noOFguesses = noOFguesses + 1
+    if (noOFguesses >10):
+        print("Game Over!!!")
